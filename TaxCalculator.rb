3303ff3ec8c8cf2b747cf2ded7b76d5ecd7b661a
Ranzreka <zach_noche@dlsu.edu.ph>
2023-03-08 10:55:39 +0800

    Initial commit

---
+++ TaxCalculator.rb
@@ -0,0 +1,152 @@
+require 'gtk3'
+require 'pango'
+require_relative 'taxform'
+class TaxCalculator < Gtk::Application
+  include TAXFORM
+  def initialize
+    super("com.example.TaxCalculator", :flags_none)
+    signal_connect "activate" do |application|
+      # Create a top-level window with a grid
+      window = Gtk::ApplicationWindow.new(application)
+      window.set_title("Tax Calculator")
+      window.set_size_request(500, 400)
+      window.set_position(:center)
+
+      #custom font
+      font_desc = Pango::FontDescription.new("Sans 14")
+      grid = Gtk::Grid.new
+      grid.set_row_spacing(10)
+      grid.set_column_spacing(10)
+      grid.set_column_homogeneous(true)
+
+      # Add a label and text field for monthly income
+      label_income = Gtk::Label.new("Monthly Income")
+      @entry_income = Gtk::Entry.new
+      label_income.override_font(font_desc)
+
+      #attach label and entry to top middle of grid
+      grid.attach(label_income, 1, 0, 1, 1)
+      grid.attach(@entry_income, 2, 0, 1, 1)
+
+      # Add a button to calculate taxes
+      button_calculate = Gtk::Button.new(:label => "Calculate")
+      button_calculate.override_font(font_desc)
+      button_calculate.signal_connect('clicked') do
+        #if empty or invalid characters display error message
+        if @entry_income.text.empty? || @entry_income.text.to_i == 0
+          dialog = Gtk::MessageDialog.new(:parent => window, :flags => :destroy_with_parent,
+                                          :type => :error, :buttons_type => :close,
+                                          :message => "Please enter a valid monthly income.")
+          dialog.run
+          dialog.destroy
+        else
+          #get monthly income
+          calculate_taxes
+        end
+      end
+      grid.attach(button_calculate, 3, 0, 1, 1)
+
+      # Tax Computation section
+      tax_label = Gtk::Label.new("Tax Computation")
+      grid.attach(tax_label, 0, 2, 1, 1)
+
+      income_tax_label = Gtk::Label.new("Income Tax")
+      grid.attach(income_tax_label, 0, 3, 1, 1)
+
+      @income_tax_entry = Gtk::Entry.new
+      @income_tax_entry.set_editable(false)
+      grid.attach(@income_tax_entry, 1, 3, 1, 1)
+
+      net_pay_label = Gtk::Label.new("Net Pay (after Income Tax)")
+      grid.attach(net_pay_label, 0, 4, 1, 1)
+
+      @net_pay_entry = Gtk::Entry.new
+      @net_pay_entry.set_editable(false)
+      grid.attach(@net_pay_entry, 1, 4, 1, 1)
+
+      # Add Monthly Contributions section
+      label_monthly_contributions = Gtk::Label.new('Monthly Contributions:')
+      grid.attach(label_monthly_contributions, 3, 1, 1, 1)
+
+      label_sss = Gtk::Label.new('SSS:')
+      grid.attach(label_sss, 3, 2, 1, 1)
+
+      @entry_sss = Gtk::Entry.new
+      @entry_sss.set_width_chars(10)
+      @entry_sss.editable = false
+      grid.attach(@entry_sss, 4, 2, 1, 1)
+
+      label_philhealth = Gtk::Label.new('Philhealth:')
+      grid.attach(label_philhealth, 3, 3, 1, 1)
+
+      @entry_philhealth = Gtk::Entry.new
+      @entry_philhealth.set_width_chars(10)
+      @entry_philhealth.editable = false
+      grid.attach(@entry_philhealth, 4, 3, 1, 1)
+
+      label_pag_ibig = Gtk::Label.new('Pag-IBIG:')
+      grid.attach(label_pag_ibig, 3, 4, 1, 1)
+
+      @entry_pag_ibig = Gtk::Entry.new
+      @entry_pag_ibig.set_width_chars(10)
+      @entry_pag_ibig.editable = false
+      grid.attach(@entry_pag_ibig, 4, 4, 1, 1)
+
+      label_total_contribution = Gtk::Label.new('Total Contribution:')
+      grid.attach(label_total_contribution, 3, 5, 1, 1)
+
+      @entry_total_contribution = Gtk::Entry.new
+      @entry_total_contribution.set_width_chars(10)
+      @entry_total_contribution.editable = false
+      grid.attach(@entry_total_contribution, 4, 5, 1, 1)
+
+      # Add Total Deduction and Net pay after deductions
+      label_total_deduction = Gtk::Label.new('Total Deduction:')
+      grid.attach(label_total_deduction, 1, 6, 1, 1)
+
+      @entry_total_deduction = Gtk::Entry.new
+      @entry_total_deduction.set_width_chars(10)
+      @entry_total_deduction.editable = false
+      grid.attach(@entry_total_deduction, 2, 6, 1, 1)
+
+      label_net_pay = Gtk::Label.new('Net Pay after Deductions:')
+      grid.attach(label_net_pay, 1, 8, 1, 1)
+
+      @entry_net_pay = Gtk::Entry.new
+      @entry_net_pay.set_width_chars(10)
+      @entry_net_pay.editable = false
+      grid.attach(@entry_net_pay, 2, 8, 1, 1)
+
+      # override font of all labels
+      labels = grid.children.select { |child| child.is_a? Gtk::Label }
+      labels.each do |label|
+        label.override_font(font_desc)
+        label.override_background_color(:normal, Gdk::RGBA.new(0.8, 0.8, 1.0, 0.8))
+      end
+      # Show the window and add the grid to it
+      window.add(grid)
+      window.signal_connect('destroy') { Gtk.main_quit }
+      window.show_all
+    end
+  end
+
+  def calculate_taxes
+    monthly_income = @entry_income.text.gsub(/[^0-9\.]/, '').to_f
+    @entry_income.text = "\u20B1 " + @entry_income.text.to_f.to_s
+    pag_ibig = getPagIBIG(monthly_income).round(2)
+    sss = getSSS(monthly_income).round(2)
+    philhealth = getPhilHealth(monthly_income).round(2)
+    total = (pag_ibig + sss + philhealth).round(2)
+
+    #limit to 3 decimal places
+    @entry_sss.text = "\u20B1 " + sss.to_s
+    @entry_philhealth.text = "\u20B1 " + philhealth.to_s
+    @entry_pag_ibig.text = "\u20B1 " + pag_ibig.to_s
+    @entry_total_contribution.text = "\u20B1 " + total.to_s
+
+  end
+end
+
+# Start the application
+app = TaxCalculator.new
+puts app.run
---
+++ taxform.rb
@@ -0,0 +1,139 @@
+# frozen_string_literal: true
+
+module TAXFORM
+
+  def getPagIBIG(monthly_income)
+
+    if monthly_income <= 1500
+      return monthly_income * 0.01
+    else
+      return monthly_income * 0.02
+    end
+  end
+  def getSSS(monthly_income)
+    case monthly_income
+    when 0...4250
+      return 4000 * 0.045
+    when 4250...4750
+      return 4500 * 0.045
+    when 4750...5250
+      return  5000 * 0.045
+    when 5250...5750
+      return 5500 * 0.045
+    when 5750...6250
+      return 6000 * 0.045
+    when 6250...6750
+      return 6500 * 0.045
+    when 6750...7250
+      return 7000 * 0.045
+    when 7250...7750
+      return 7500 * 0.045
+    when 7750...8250
+      return 8000 * 0.045
+    when 8250...8750
+      return 8500 * 0.045
+    when 8750...9250
+      return 9000 * 0.045
+    when 9250...9750
+      return 9500 * 0.045
+    when 9750...10250
+      return 10000 * 0.045
+    when 10250...10750
+      return 10500 * 0.045
+    when 10750...11250
+      return 11000 * 0.045
+    when 11250...11750
+      return 115000 * 0.045
+    when 11750...12250
+      return 12000 * 0.045
+    when 12250...12750
+      return 12500 * 0.045
+    when 12750...13250
+      return 13000 * 0.045
+    when 13250...13750
+      return 13500 * 0.045
+    when 13750...14250
+      return 14000 * 0.045
+    when 14250...14750
+      return 14500 * 0.045
+    when 14750...15250
+      return 15000 * 0.045
+    when 15250...15750
+      return 15500 * 0.045
+    when 15750...16250
+      return 16000 * 0.045
+    when 16250...16750
+      return 16500 * 0.045
+    when 16750...17250
+      return 17000 * 0.045
+    when 17250...17750
+      return 17500 * 0.045
+    when 17750...18250
+      return 18000 * 0.045
+    when 18250...18750
+      return 18500 * 0.045
+    when 18750...19250
+      return 19000 * 0.045
+    when 19250...19750
+      return 19500 * 0.045
+    when 19750...20250
+      return 20000 * 0.045
+    when 20250...20750
+      return 20500 * 0.045
+    when 20750...21250
+      return 21000 * 0.045
+    when 21250...21750
+      return 21500 * 0.045
+    when 21750...22250
+      return 22000 * 0.045
+    when 22250...22750
+      return 22500 * 0.045
+    when 22750...23250
+      return 23000 * 0.045
+    when 23250...23750
+      return 23500 * 0.045
+    when 23750...24250
+      return 24000 * 0.045
+    when 24250...24750
+      return 24500 * 0.045
+    when 24750...25250
+      return 25000 * 0.045
+    when 25250...25750
+      return 25500 * 0.045
+    when 25750...26250
+      return 26000 * 0.045
+    when 26250...26750
+      return 26500 * 0.045
+    when 26750...27250
+      return 27000 * 0.045
+    when 27250...27750
+      return 27500 * 0.045
+    when 27750...28250
+      return 28000 * 0.045
+    when 28250...28750
+      return 28500 * 0.045
+    when 28750...29250
+      return 29000 * 0.045
+    when 29250...29750
+      return 29500 * 0.045
+    else
+      return 30000 * 0.045
+    end
+  end
+
+  def getPhilHealth(monthly_income)
+    # contribution is set at 4.5%
+    case monthly_income
+    when 0..10000
+      return 10000 * 0.045 / 2
+    when 10000...90000
+      return monthly_income * 0.045 / 2
+    else
+      return 4050
+    end
+  end
+
+  def getTotalContributions(sss, philhealth, pagibig)
+    return sss + philhealth + pagibig
+  end
+end
